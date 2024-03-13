0d247e3f1417f0187d10b0237c7454750d0b76b8
Ahzam Akhtar <ahzam8186@gmail.com>
2024-03-13 13:22:36 +0530

    Random Games Section Updated

--- pages/RandomGame.js
+++ pages/RandomGame.js
@@ -1,6 +1,51 @@
-import React from 'react'
-
+import Link from 'next/link'
+import React, { useRef, useState } from 'react'
 const RandomGame = () => {
+    const [gameLink, setLink] = useState("")
+    const refOne = useRef()
+    const refTwo = useRef()
+    const refThree = useRef()
+    const refFour = useRef()
+    const refButton = useRef()
+
+    const handleRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 5);
+        switch (randomNumber) {
+            case 1:
+                refOne.current.style.backgroundColor = "red"
+                refTwo.current.style.backgroundColor = "#374151"
+                refThree.current.style.backgroundColor = "#374151"
+                refFour.current.style.backgroundColor = "#374151"
+                refButton.current.style.backgroundColor = "red"
+                setLink("/Games/TowerDefence")
+                break;
+            case 2:
+                refTwo.current.style.backgroundColor = "yellow"
+                refOne.current.style.backgroundColor = "#374151"
+                refThree.current.style.backgroundColor = "#374151"
+                refFour.current.style.backgroundColor = "#374151"
+                refButton.current.style.backgroundColor = "yellow"
+                setLink("/Games/MoleSmash")
+                break;
+            case 3:
+                refThree.current.style.backgroundColor = "green"
+                refOne.current.style.backgroundColor = "#374151"
+                refTwo.current.style.backgroundColor = "#374151"
+                refFour.current.style.backgroundColor = "#374151"
+                refButton.current.style.backgroundColor = "green"
+                setLink("/Games/SkylineSkaddle")
+                break;
+            case 4:
+                refFour.current.style.backgroundColor = "orange"
+                refOne.current.style.backgroundColor = "#374151"
+                refTwo.current.style.backgroundColor = "#374151"
+                refThree.current.style.backgroundColor = "#374151"
+                refButton.current.style.backgroundColor = "orange"
+                setLink("/Games/CoinFlip")
+                break;
+        }
+    }
+
     return (
         <>
             <div className='flex justify-center items-center'>
@@ -11,28 +56,35 @@ const RandomGame = () => {
             <div style={{ backgroundColor: "#191414", borderColor: "yellow", borderWidth: 3, borderRadius: 5 }} className='rounded-md'>
 
                 <div class="grid grid-cols-2 mt-10">
-                    <div className='flex flex-row'>
-                        <div style={{ width: 100, height: 100, alignItems: "center" }} className='flex justify-center bg-red-500'>
+                    <div className='flex flex-row rotate-6'>
+                        <div style={{ width: 100, height: 100, alignItems: "center" }} className='flex justify-center bg-red-500 border-4 border-white'>
                             <h1 className='font-abc text-white text-6xl'>1</h1>
                         </div>
-                        <img src='/ca11.png' height={300} width={300} alt='' />
+                        <div className='flex justify-center items-center bg-red-500'>
+                            <img src='/ca11.png' height={300} width={300} alt='' />
+                        </div>
                     </div>
                     <div className='-rotate-12'>
-                        <img src='/ca22.png' height={300} width={300} alt='' />
-                        <div style={{ width: 100, height: 100, alignItems: "center" }} className='flex justify-center bg-yellow-500'>
+                        <div className='bg-yellow-400 flex justify-center'>
+                            <img src='/ca22.png' height={300} width={300} alt='' />
+                        </div>
+                        <div style={{ width: 100, height: 100, alignItems: "center" }} className='flex justify-center bg-yellow-500 border-4 border-white'>
                             <h1 className='font-abc text-white text-6xl'>2</h1>
                         </div>
                     </div>
                     <div className='-rotate-12'>
-                    <div style={{ width: 100, height: 100, alignItems: "center" }} className='flex justify-center bg-green-500'>
+                        <div style={{ width: 100, height: 100, alignItems: "center" }} className='flex justify-center bg-green-500 border-4 border-white'>
                             <h1 className='font-abc text-white text-6xl'>3</h1>
                         </div>
-                        <img src='/ca33.png' height={300} width={300} alt='' />
+                        <div className='bg-green-500 flex justify-center'>
+                            <img src='/ca33.png' height={300} width={300} alt='' />
+                        </div>
                     </div>
                     <div>
-                    
-                        <img src='/ca44.png' height={300} width={300} alt='' />
-                        <div style={{ width: 100, height: 100 , alignItems: "center" }} className='flex justify-center bg-orange-500'>
+                        <div className='bg-orange-500 flex justify-center'>
+                            <img src='/ca44.png' height={300} width={300} alt='' />
+                        </div>
+                        <div style={{ width: 100, height: 100, alignItems: "center" }} className='flex justify-center bg-orange-500 border-4 border-white'>
                             <h1 className='font-abc text-white text-6xl'>4</h1>
                         </div>
                     </div>
@@ -40,24 +92,26 @@ const RandomGame = () => {
                 <div id="bottom" className='flex justify-center mt-14'>
                     <div style={{ width: '100%', maxWidth: '500px' }} className='flex flex-row h-24 bg-black items-center rounded-lg border-2 border-white'>
                         <div className='items-center m-1'>
-                            <button type="button" class="py-5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border-2 border-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Choose</button>
+                            <button onClick={handleRandom} type="button" class="py-5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border-2 border-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Choose</button>
                         </div>
-                        <div id="red" style={{ width: "25%", height: "70%", margin: 5 }} className='bg-red-500 items-center flex justify-center border-4 border-white'>
-                            <h1 className='font-abc text-white text-6xl'>?</h1>
+                        <div ref={refOne} id="red" style={{ width: "25%", height: "70%", margin: 5 }} className='bg-gray-700 items-center flex justify-center border-4 border-white'>
+                            <h1 className='font-abc text-gray-700 text-6xl'>1</h1>
 
                         </div>
-                        <div style={{ width: "25%", height: "70%", margin: 5 }} className='bg-orange-500 items-center flex justify-center border-4 border-white'>
-                            <h1 className='font-abc text-white text-6xl'>?</h1>
+                        <div ref={refTwo} style={{ width: "25%", height: "70%", margin: 5 }} className='bg-gray-700 items-center flex justify-center border-4 border-white'>
+                            <h1 className='font-abc text-gray-700 text-6xl'>2</h1>
 
                         </div>
-                        <div style={{ width: "25%", height: "70%", margin: 5 }} className='bg-green-500 items-center flex justify-center border-4 border-white'>
-                            <h1 className='font-abc text-white text-6xl'>?</h1>
+                        <div ref={refThree} style={{ width: "25%", height: "70%", margin: 5 }} className='bg-gray-700 items-center flex justify-center border-4 border-white'>
+                            <h1 className='font-abc text-gray-700 text-6xl'>3</h1>
                         </div>
-                        <div style={{ width: "25%", height: "70%", margin: 5 }} className='bg-yellow-400 items-center flex justify-center border-4 border-white'>
-                            <h1 className='font-abc text-white text-6xl'>?</h1>
+                        <div ref={refFour} style={{ width: "25%", height: "70%", margin: 5 }} className='bg-gray-700 items-center flex justify-center border-4 border-white'>
+                            <h1 className='font-abc text-gray-700 text-6xl'>4</h1>
                         </div>
                         <div className='items-center m-1'>
-                            <button type="button" class="py-5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">PlayNow</button>
+                            <Link href={gameLink}>
+                                <button ref={refButton} type="button" class="py-5 px-5 me-2 mb-2 font-medium font-abc text-lg focus:outline-none bg-gray-600 rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-100 text-black">PlayNow</button>
+                            </Link>
                         </div>
                     </div>
 
