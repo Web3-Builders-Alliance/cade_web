c6c852581f167e7773549bfe1584b768c9b87d35
Ahzam Akhtar <ahzam8186@gmail.com>
2024-03-16 18:10:45 +0530

    Prize Enhancements

--- pages/Prize.js
+++ pages/Prize.js
@@ -107,22 +107,22 @@ const GetCade = ({ network }) => {
     return (
         <>
             <section className="min-h-screen flex justify-center text-gray-600 body-font overflow-x-hidden bg-gradient-to-bl from-blue-950 via-black to-black">
-                <div className="p-10 flex justify-center">
-                    <div className="rounded-xl  xl:p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-x-5 w-screen">
+                <div className="px-10 flex justify-center">
+                    <div className="rounded-xl  xl:p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-x-5 w-screen p-10 gap-y-10">
                         <div id="first" className="border-4 border-white rounded-xl bg-[url('/brickwall.jpg')]  flex flex-col md:ml-auto w-full h-max">
                             <CadeStore />
 
                         </div>
-                     
+
                         <div id="second" className=" items-center border-4 rounded-xl border-white bg-gradient-to-bl from-blue-950 via-black to-black  flex flex-col md:ml-auto w-full  overflow-x-hidden overflow-y-hidden">
-                        <div className="">
-                        <h1 className="text-white font-abc text-5xl mt-5 underline">More Options</h1>
-                        </div>
-                            <div style={{ width: '80%', height: 160 }} className="mt-5 bg-gray-700 border-4 border-white rounded-md">
+                            <div className="">
+                                <h1 className="text-white font-abc text-5xl mt-5 underline">More Options</h1>
+                            </div>
+                            <div style={{ width: '80%'}} className="mt-5 bg-gray-700 border-4 border-white rounded-md">
                                 <div className="flex items-center justify-center mt-5">
-                                <div className="m-2 rounded-full w-4 h-4 bg-green-500 animate-blink"></div>
+                                    <div className="m-2 rounded-full w-4 h-4 bg-green-500 animate-blink"></div>
                                     <h1 className="text-yellow-300 text-4xl font-abc">
-                                        Selected Item Name - Cade Game Pass
+                                        Item Name - Cade GamePass
                                     </h1>
                                 </div>
                                 <div className="flex items-center justify-center mt-5 ml-2">
@@ -134,15 +134,18 @@ const GetCade = ({ network }) => {
 
                             <div className="p-10 text-gray-400 body-font">
                                 <div className="container px-5 py-2 mx-auto">
-                                    <div className="flex flex-wrap -m-4">
-                                        <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                                    <div className="flex flex-wrap -m-4 bg-gray-900 rounded-lg border-2 border-white    ">
+                                        <div className="lg:w-1/3 lg:mb-0 mb-6 p-4 ">
                                             <div className="h-full text-center">
                                                 <div className="flex justify-center">
                                                     <img src="/cadenew.png" alt="CadeImage" />
                                                 </div>
                                                 <p className="text-white  font-abc text-2xl">20x Cade Coins</p>
+                                                <button type="button" class="mt-2 text-black bg-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-10 py-2.5 me-2 mb-2 dark:bg-white dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redeem</button>
                                             </div>
-                                            
+
+
+
                                         </div>
                                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                                             <div className="h-full text-center">
@@ -150,8 +153,9 @@ const GetCade = ({ network }) => {
                                                     <img src="/cadenew.png" alt="CadeImage" />
                                                 </div>
                                                 <p className="text-white  font-abc text-2xl">40x Cade Coins</p>
+                                                <button type="button" class="mt-2 text-black bg-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-10 py-2.5 me-2 mb-2 dark:bg-white dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redeem</button>
                                             </div>
-                                            
+
                                         </div>
                                         <div className="lg:w-1/3 lg:mb-0 p-4">
                                             <div className="h-full text-center">
@@ -159,18 +163,19 @@ const GetCade = ({ network }) => {
                                                     <img src="/cadenew.png" alt="CadeImage" />
                                                 </div>
                                                 <p className="text-white  font-abc text-2xl">60x Cade Coins</p>
+                                                <button type="button" class="mt-2 text-black bg-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-10 py-2.5 me-2 mb-2 dark:bg-white dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redeem</button>
                                             </div>
-                                           
+
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className="container px-5 py-10 mx-auto">
-                                    <div className="flex flex-wrap -m-4">
+                                    <div className="flex justify-center flex-wrap -m-4 bg-gray-900 rounded-lg border-2 border-white">
                                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                                             <div className="h-full text-center">
                                                 <div className="flex justify-center">
-                                                    <div className="bg-black sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full  flex-shrink-0">
+                                                    <div className="ng-black sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
                                                         <img
                                                             height={100}
                                                             width={100}
@@ -181,8 +186,9 @@ const GetCade = ({ network }) => {
                                                     </div>
                                                 </div>
                                                 <p className="text-white  font-abc text-2xl">20x CadeLife</p>
+                                                <button type="button" class="mt-2 text-black bg-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-10 py-2.5 me-2 mb-2 dark:bg-white dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redeem</button>
                                             </div>
-                                    
+
                                         </div>
                                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                                             <div className="h-full text-center">
@@ -198,8 +204,9 @@ const GetCade = ({ network }) => {
                                                     </div>
                                                 </div>
                                                 <p className="text-white  font-abc text-2xl">40x CadeLife</p>
+                                                <button type="button" class="mt-2 text-black bg-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-10 py-2.5 me-2 mb-2 dark:bg-white dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redeem</button>
                                             </div>
-                                            
+
                                         </div>
                                         <div className="lg:w-1/3 lg:mb-0 p-4">
                                             <div className="h-full text-center">
@@ -215,8 +222,9 @@ const GetCade = ({ network }) => {
                                                     </div>
                                                 </div>
                                                 <p className="text-white  font-abc text-2xl">60x CadeLife</p>
+                                                <button type="button" class="mt-2 text-black bg-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-10 py-2.5 me-2 mb-2 dark:bg-white dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redeem</button>
                                             </div>
-                                           
+
                                         </div>
                                     </div>
                                 </div>
